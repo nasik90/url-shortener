244025ca25fc563c578b14c2a255ff97e138e1f9
nasik90 <anasnazm@gmail.com>
2025-01-05 00:52:54 +0300

    get done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,32 +8,48 @@ import (
 	"github.com/nasik90/url-shortener/internal/app/handlers"
 )
 
+const shortURLlen = 8
+
+var urlCache = make(map[string]string)
+
 func mainPage(res http.ResponseWriter, req *http.Request) {
 	var buf bytes.Buffer
 	_, err := buf.ReadFrom(req.Body)
 	if err != nil {
 		//res.Write([]byte(err.Error()))
-		http.Error(res, err.Error(), http.StatusInternalServerError)
+		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
 	url := buf.String()
-	randomString, err := handlers.RandomString(8)
+	randomString, err := handlers.RandomString(shortURLlen)
 	if err != nil {
 		//res.Write([]byte(err.Error()))
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	shortUrl := req.Host + "/" + randomString
-	urlCache := make(map[string]string)
-	urlCache[shortUrl] = url
+	shortURL := req.Host + "/" + randomString
+	//urlCache := make(map[string]string)
+	urlCache[shortURL] = url
 	res.Header().Set("content-type", "text/plain")
-	res.Header().Set("Content-Length", strconv.Itoa(len(shortUrl)))
+	res.Header().Set("Content-Length", strconv.Itoa(len(shortURL)))
 	res.WriteHeader(http.StatusCreated)
-	res.Write([]byte(shortUrl))
+	res.Write([]byte(shortURL))
 }
 
 func urlShort(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("Привет!"))
+	id := req.RequestURI
+	if len(id) == shortURLlen+1 {
+		id = id[1:]
+	}
+	shortURL := req.Host + "/" + id
+	originalURL, ok := urlCache[shortURL]
+	if !ok {
+		http.Error(res, "nothing found", http.StatusBadRequest)
+		return
+	}
+	res.Header().Set("Location", originalURL)
+	res.WriteHeader(http.StatusTemporaryRedirect)
+	//res.Write([]byte(id))
 }
 
 func main() {
