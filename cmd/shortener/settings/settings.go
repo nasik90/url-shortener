96c7dd52a529de56478d3d7bc522f5f2a43856ef
nasik90 <anasnazm@gmail.com>
2025-03-09 19:46:59 +0300

    go tests again

--- cmd/shortener/settings/settings.go
+++ cmd/shortener/settings/settings.go
@@ -35,8 +35,8 @@ func ParseFlags(o *Options) {
 	flag.StringVar(&o.BaseURL, "b", "http://localhost:8080", "base address for short URL")
 	flag.StringVar(&o.LogLevel, "l", "debug", "log level")
 	flag.StringVar(&o.FilePath, "f", "URLStorage.txt", "file storage path")
-	flag.StringVar(&o.DatabaseDSN, "d", "host=localhost user=postgres password=xxxx dbname=URLShortener sslmode=disable", "database connection string")
-	//flag.StringVar(&o.DatabaseDSN, "d", "", "database connection string")
+	//flag.StringVar(&o.DatabaseDSN, "d", "host=localhost user=postgres password=xxxx dbname=URLShortener sslmode=disable", "database connection string")
+	flag.StringVar(&o.DatabaseDSN, "d", "", "database connection string")
 	flag.Parse()
 
 	if serverAddress := os.Getenv("SERVER_ADDRESS"); serverAddress != "" {
