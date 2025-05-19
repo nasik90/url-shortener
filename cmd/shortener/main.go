98831216b005e804162c5a456c4618075192a7d4
nasik90 <anasnazm@gmail.com>
2025-05-19 16:03:57 +0300

    errors is fixed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,8 +10,6 @@ import (
 	"sync"
 	"syscall"
 
-	_ "net/http/pprof"
-
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/nasik90/url-shortener/cmd/shortener/settings"
 	handler "github.com/nasik90/url-shortener/internal/app/handlers"
