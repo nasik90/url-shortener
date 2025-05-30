7c37d0abeb1647d3ca441b2f611688d71466ec32
nasik90 <anasnazm@gmail.com>
2025-05-30 23:33:46 +0300

    вернул примеры как было

--- internal/app/handlers/example_test.go
+++ internal/app/handlers/example_test.go
@@ -5,26 +5,19 @@ import (
 	"io"
 	"net/http"
 	"strings"
-
-	"github.com/nasik90/url-shortener/cmd/shortener/settings"
 )
 
 func ExampleHandler_GetShortURL() {
 
-	options := new(settings.Options)
-	settings.ParseFlags(options)
-
-	originalURL := "http://ya.ru"
-	endpoint := options.ServerAddress
-	client := &http.Client{CheckRedirect: func(req *http.Request, via []*http.Request) error {
-		// Возвращаем ошибку, чтобы отключить перенаправление
-		return http.ErrUseLastResponse
-	}}
-	// Запросим короткий URL
-	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(originalURL))
+	shortURL := "ya.ru"
+	endpoint := "http://localhost:8080/"
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(shortURL))
 	if err != nil {
 		panic(err)
 	}
+	// в заголовках запроса указываем кодировку
+	// request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	response, err := client.Do(request)
 	if err != nil {
 		panic(err)
@@ -34,42 +27,16 @@ func ExampleHandler_GetShortURL() {
 	if err != nil {
 		panic(err)
 	}
-	defer response.Body.Close()
-	fmt.Println(response.StatusCode)
-	responseBody := string(body)
+	fmt.Println(string(body))
 
-	// Запросим оригинальный URL для проверки, что вернется корректный URL
-	request, err = http.NewRequest(http.MethodGet, responseBody, nil)
-	if err != nil {
-		panic(err)
-	}
-	response, err = client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	originalURL = response.Header.Get("Location")
-	fmt.Println(response.StatusCode)
-	fmt.Println(originalURL)
-
-	// Output:
-	// 201
-	// 307
-	// http://ya.ru
 }
 
 func ExampleHandler_GetOriginalURL() {
 
-	options := new(settings.Options)
-	settings.ParseFlags(options)
-
-	originalURL := "http://habr.ru"
-	endpoint := options.BaseURL
-	client := &http.Client{CheckRedirect: func(req *http.Request, via []*http.Request) error {
-		// Возвращаем ошибку, чтобы отключить перенаправление
-		return http.ErrUseLastResponse
-	}}
-	// Запросим короткий URL, чтобы в дальнейшем получить оригинальный URL
-	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(originalURL))
+	endpoint := "http://localhost:8080/qwErty12"
+	client := &http.Client{}
+	// Запросим оригинальный URL
+	request, err := http.NewRequest(http.MethodGet, endpoint, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -78,28 +45,7 @@ func ExampleHandler_GetOriginalURL() {
 		panic(err)
 	}
 	defer response.Body.Close()
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(response.StatusCode)
-	responseBody := string(body)
-
-	// Запросим оригинальный URL
-	request, err = http.NewRequest(http.MethodGet, responseBody, nil)
-	if err != nil {
-		panic(err)
-	}
-	response, err = client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	originalURL = response.Header.Get("Location")
+	originalURL := response.Header.Get("Location")
 	fmt.Println(response.StatusCode)
 	fmt.Println(originalURL)
-
-	// Output:
-	// 201
-	// 307
-	// http://habr.ru
 }
